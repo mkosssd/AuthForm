ff54719c0b27828f41ffa7c4b2cfda170fbc5ce8
Manas Khandelwal <manas.khandelwal@sltech.local>
2023-09-14 15:43:20 +0530

    Worked on UI

---
+++ logged.js
@@ -0,0 +1,30 @@
+let nameDiv = document.getElementById("name");
+
+let emailDiv = document.getElementById("mail");
+
+let box = document.getElementById("userDetail");
+
+let user = localStorage.getItem("user");
+
+let logoutDiv = document.getElementById("logout");
+
+if (user === null) {
+	box.innerHTML = `<h5 class="fw-bold">User not logged in redirecting you to sign up page...</h5>`;
+
+	setTimeout(() => {
+		location.href = "/";
+	}, 3000);
+} else {
+	let userData = JSON.parse(user);
+
+	logoutDiv.innerHTML = ` <button class="btn btn-primary rounded-pill py-3 px-2 fw-bold col-12" onclick="logout()">Logout</button> `;
+
+	nameDiv.innerHTML = `<p class="display-6 fw-semibold">Name: ${userData.firstName} ${userData.lastName}</p>`;
+
+	emailDiv.innerHTML = `<p class="h4 fw-semibold">Email: ${userData.email}</p>`;
+}
+const logout = () => {
+	localStorage.removeItem("user");
+
+	location.href = "/";
+};
--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const log = () => {
+	console.log("ss");
+};
+let inValidSpan = document.getElementById('inValid')
 const formOp = (e, method) => {
 	e.preventDefault();
 	let form = e.target;
@@ -6,34 +10,51 @@ const formOp = (e, method) => {
 		let lastName = form.lastName.value;
 		let email = form.Email.value;
 		let password = form.inputPassword.value;
+		if(firstName===''||lastName===''||email===''||password===''||form.confirmPassword.value===''){
+			inValidSpan.innerText = 'Form details not filled correctly'
+			return
+		}
 		const data = {
 			firstName,
 			lastName,
 			email,
 			password,
 		};
+
 		storeData(data);
-	} else {
-		console.log("login");
+	} else if (method === "login") {
 		let mail = form.Email.value;
+		checkUser(mail);
+	} else {
 		let password = form.inputPassword.value;
-		checkUser(mail, password);
+		checkPassword(password);
 	}
+	form.reset();
+	inValidSpan.innerText=''
 };
 const validator = (params) => {
 	let par = document.getElementById(params);
+
 	let value = par.value;
+
 	let text = par.previousElementSibling.innerHTML;
-	let regEx = /^[a-zA-Z0-9!@#$%^&*]{8}$/;
+
+	let re =
+		/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+
 	let numEx = /\d/;
+
 	let errorSpan = document.getElementById(
 		"error" + params
 	);
-	if (value === "")
+	if (value === "") {
 		errorSpan.innerText =
 			text + " cannot be empty";
+		
+	}
 
 	if (value !== "") errorSpan.innerText = "";
+
 	if (
 		params === "firstName" ||
 		params === "lastName"
@@ -41,69 +62,101 @@ const validator = (params) => {
 		if (numEx.test(value))
 			errorSpan.innerText =
 				"Name cannot contain numbers";
+		
 	}
+
 	if (params === "confirmPassword") {
 		let password = document.getElementById(
 			"inputPassword"
 		).value;
+
 		if (password !== value) {
 			errorSpan.innerText =
 				"Both password should match";
+			
 		} else {
 			errorSpan.innerText = "";
 		}
 	}
 	if (params === "inputPassword") {
-		var re =
-			/^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 		if (!re.test(value)) {
 			errorSpan.innerHTML =
-				"Password should match the criteria and should have 8 chars";
+			"Password should match the criteria and should have 8 chars";
 		}
 	}
 	if (params === "Email") {
 		if (
 			!(
 				value.includes("@") && value.includes(".")
-			)
-		)
-			errorSpan.innerHTML = "Enter a valid email";
+				)
+				) {
+					errorSpan.innerHTML = "Enter a valid email";
+				}
 	}
 };
+let user;
 const userData =
 	localStorage.getItem("users") || [];
+
 const parsedData = JSON.parse(userData);
+
 const storeData = (data) => {
 	parsedData.push(data);
 
 	const user = JSON.stringify(parsedData);
+
 	localStorage.setItem("users", user);
 };
-const checkUser = (email, password) => {
-	let nameDiv = document.getElementById("name");
-
-	let emailDiv = document.getElementById("mail");
-
-	let userDetail =
-		document.getElementById("userDetail");
-
+const checkUser = (email) => {
 	let errorSpan =
 		document.getElementById("error");
+
 	if (parsedData.find((e) => e.email === email)) {
-		const user = parsedData.filter(
+		document.getElementById("box").innerHTML =
+			passwordBox;
+
+		user = parsedData.filter(
 			(e) => e.email === email
 		)[0];
-		if (user.password === password) {
-			userDetail.classList.remove("d-none");
-			errorSpan.innerText = "Logged IN!";
-			nameDiv.innerHTML = `<p>Name: ${user.firstName} ${user.lastName}</p>`;
-			emailDiv.innerHTML = `<p>Email: ${user.email}</p>`;
-		} else {
-			errorSpan.innerText = "WRONG PASS!";
-			userDetail.classList.add("d-none");
-		}
 	} else {
-		errorSpan.innerText = "WRONG EMAIl!";
-		userDetail.classList.add("d-none");
+		errorSpan.innerText = "ENTER CORRECT EMAIL!";
 	}
 };
+
+const checkPassword = (password) => {
+	let errorSpan =
+		document.getElementById("error");
+
+	if (user.password === password) {
+		location.href = "/logged.html";
+
+		localStorage.setItem(
+			"user",
+			JSON.stringify(user)
+		);
+	} else {
+		errorSpan.innerText =
+			"ENTER CORRECT PASSWORD!";
+	}
+};
+
+var passwordBox = ` <form id="form" onsubmit="formOp(event,'password')">
+<div class="col-11 ">
+<label for="inputPassword" class="col-sm-5 col-form-label fw-semibold">Password</label>
+<input
+type="password"
+class="form-control rounded-4 py-3"
+id="inputPassword"
+placeholder="Enter Password"
+minlength="8"
+required
+>
+<span class="small text-danger fw-semibold" id="error"></span>
+</div>
+
+
+<div class="col-11 justify-content-between d-flex my-4 px-4">
+<a class="btn btn-secondary fw-bold col-5 my-3  py-3 rounded-pill px-2" href="/">Change Method</a>
+<button class="btn btn-primary col-5 my-3 rounded-pill py-3 px-2 fw-bold" type="submit" id="signup">Next</button>
+</div>
+</form>`;
